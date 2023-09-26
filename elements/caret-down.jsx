69d636906ce9bef79c1c789152dc6730d155f7d4
sadiq-1698 <mohammedsadiq1698@gmail.com>
2023-09-26 08:04:58 +0530

    added new elements

---
+++ elements/caret-down.jsx
@@ -0,0 +1,15 @@
+const CaretDown = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      focusable="false"
+      class="Q6yead QJZfhe mig17c "
+    >
+      <path d="M7 10l5 5 5-5H7z"></path>
+    </svg>
+  );
+};
+
+export default CaretDown;
