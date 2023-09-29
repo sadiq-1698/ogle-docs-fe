d94cdc6f35ab3c2588fc5c3eaa9001f8bd74f57b
sadiq-1698 <mohammedsadiq1698@gmail.com>
2023-09-29 16:38:01 +0530

    notify people UI

--- components/modal/share-doc/notify-people.jsx
+++ components/modal/share-doc/notify-people.jsx
@@ -1,3 +1,4 @@
+import CaretDown from "@/elements/caret-down";
 import Close from "@/elements/close";
 import { useEffect } from "react";
 
@@ -18,9 +19,12 @@ const NotifyPeople = ({ usersToNotify, setNotifyScreen, setUsersToNotify }) => {
     <div>
       <div className="flex items-center px-6">
         <div className="h-12 py-1 flex-1 border-2 border-solid border-gray-400 focus:border-blue-700 px-4 rounded-md flex flex-wrap">
-          {usersToNotify.map((user, index) => {
+          {usersToNotify.map((user) => {
             return (
-              <div className="border border-gray-400 rounded-3xl bg-blue-300 px-2 py-1 flex items-center">
+              <div
+                key={user._id}
+                className="border border-gray-400 rounded-3xl bg-grey-7 px-2 flex items-center"
+              >
                 <span className="text-sm mr-1">{user.name}</span>
                 <button onClick={() => handleUserCloseClick(user)}>
                   <Close size={18} />
@@ -29,9 +33,26 @@ const NotifyPeople = ({ usersToNotify, setNotifyScreen, setUsersToNotify }) => {
             );
           })}
         </div>
-        <div className="h-12 p-0.5 w-1/6 ml-2 border-2 border-solid border-gray-400 focus:border-blue-700 px-4 rounded-md">
+        <button className="h-12 p-0.5 w-1/5 ml-2 border-2 border-solid border-gray-400 px-4 rounded-md flex items-center hover:bg-grey-6">
           Role
-        </div>
+          <CaretDown />
+        </button>
+      </div>
+
+      <div className="my-4 px-6">
+        <textarea
+          placeholder="Message"
+          className="w-full border-2 border-solid border-gray-400 focus:border-blue-700 outline-none rounded-md p-3 px-4"
+        />
+      </div>
+
+      <div className="flex items-center justify-end px-6">
+        <button className="text-sm text-blue-700 px-3 py-2 mr-3 hover:bg-blue-100 rounded-3xl font-semibold">
+          Cancel
+        </button>
+        <button className="text-sm text-white bg-blue-700 hover:bg-blue-900 px-3 py-2 rounded-3xl font-semibold">
+          Send
+        </button>
       </div>
     </div>
   );
